aaae69193378800c39c5f353864a807adb298284
kiptoo haron <kiptooharon.hk@gmail.com>
2024-02-12 15:19:00 +0300

    Revert "added project readme file"
    This reverts commit 86c237a44d36b81ccf4f8986b383841dd6aaf9fe.

--- b.py
+++ b.py
@@ -4,12 +4,6 @@
 import sys
 import json
 from io import StringIO
-from models import user
-from models import base_model
-from models import place
-from models import city
-from models import state
-from models import amenity
 from models import review
 import unittest
 from unittest.mock import patch
@@ -19,30 +13,24 @@ class test_console(unittest.TestCase):
     """ this class test console """
 
     def setUp(self):
-        self.User = user.User
-        self.BaseModel = base_model.BaseModel
-        self.Place = place.Place
-        self.City = city.City
-        self.State = state.State
-        self.Amenity = amenity.Amenity
         self.Review = review.Review
 
-    def test_user_model(self):
-        """test user creation"""
+    def test_Review_model(self):
+        """test Review creation"""
         jstr = ''
         with patch('sys.stdout', new=StringIO()) as f:
-            """creating first user """
+            """creating first Review """
 
-            console.HBNBCommand().onecmd("User.creat()")
+            console.HBNBCommand().onecmd("Review.creat()")
         captured_id1 = f.getvalue()
         self.assertTrue(isinstance(captured_id1, str), True)
         self.assertEqual(len(captured_id1), 37) 
         with open("file.json", "r") as file:
             jstr = json.load(file)
         dic = eval(repr(jstr))
-        obj_key = "User." + captured_id1[:-1]
+        obj_key = "Review." + captured_id1[:-1]
         obj_dict1 = dic[obj_key]
-        recreated_obj = self.User(**obj_dict1)
+        recreated_obj = self.Review(**obj_dict1)
             #recreatin object with object dictionary
         dict2 = recreated_obj.to_dict()
             #comparing if new object dictionary == existing obj dictionaty
@@ -50,23 +38,24 @@ class test_console(unittest.TestCase):
         self.assertTrue(dict2 == obj_dict1)
 
         with patch('sys.stdout', new=StringIO()) as f:
-            """creating second user """
+            """creating second Review """
 
-            console.HBNBCommand().onecmd("User.creat()")
+            console.HBNBCommand().onecmd("Review.creat()")
  
         captured_id2 = f.getvalue()
         with open("file.json", "r") as file:
             jstr = json.load(file)
-        obj_key2 = "User." + captured_id2[:-1]
+        obj_key2 = "Review." + captured_id2[:-1]
        	obj_dict2 = eval(repr(jstr))
-        all_str = str(dic[obj_key]) + '\n' + str(obj_dict2[obj_key2]) + '\n'
+        all_str = str(dic[obj_key]) + '\n' + str(obj_dict2[obj_key2]) + '\n' 
 
-        with patch ('sys.stdout', new=StringIO()) as f:
-            console.HBNBCommand().onecmd("User.count()")
+        """ test Review.all() """
 
-        count = f.getvalue()
-        self.assertEqual(count, "2\n")
-
+        with patch('sys.stdout', "r") as f:
+            console.HBNBCommand().onecmd("User.all()")
+#        captured = f.getvalue()
+ #       print(captured)
+       
 
 if __name__ == "__main__":
     unittest.main()
